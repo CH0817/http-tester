77358cfb7d7a244ac692f43109ca82756c591449
rex <yu.chenhang@gmail.com>
2022-12-08 23:04:31 +0800

    取 token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'http-tester';
+
+  constructor(private http: HttpClient) {}
+
+  async ngOnInit() {
+    await this.getToken();
+  }
+
+  async getToken() {
+    const header = {
+      headers: new HttpHeaders()
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .set('Authorization', 'Basic ' + btoa('oauth:oauth')),
+    };
+
+    const params = new URLSearchParams();
+    params.set('grant_type', 'password');
+    params.set('username', 'rex');
+    params.set('password', '1');
+    params.set('scope', 'all');
+
+    this.http
+      .post('/auth/oauth/token', params.toString(), header)
+      .pipe(map((data) => data))
+      .subscribe((data) => localStorage.setItem('token', JSON.stringify(data)));
+  }
+
+  send() {}
 }
